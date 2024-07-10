0518f22c403fd99eebe7fc0b1f16f98179156e25
Dejvid Ugrinovski <dejvidu@nextsense.com>
2024-07-10 16:07:37 +0200

    add framework

---
+++ TetovskiPod.framework/Headers/TetovskiPod.h
@@ -0,0 +1,18 @@
+//
+//  TetovskiPod.h
+//  TetovskiPod
+//
+//  Created by Dejvid Ugrinovski on 10.7.24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TetovskiPod.
+FOUNDATION_EXPORT double TetovskiPodVersionNumber;
+
+//! Project version string for TetovskiPod.
+FOUNDATION_EXPORT const unsigned char TetovskiPodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TetovskiPod/PublicHeader.h>
+
+
